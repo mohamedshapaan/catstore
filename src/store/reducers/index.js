5a6a5dca0e1ca2998be911a24a4891a0a9a80e3f
mohamedshapaan <mshapaan959@gmail.com>
2020-09-10 02:31:22 +0200

    catstore

---
+++ src/store/reducers/index.js
@@ -0,0 +1,36 @@
+import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from "../actions/types";
+
+export default function cartReduer(state, action){
+    let new_state
+    switch(action.type){
+        case ADD_TO_CART: {
+            return [
+                ...state,
+                {
+                    product: action.productInfo,
+                    quantity: action.quantity
+                }
+            ]
+        }
+
+        case REMOVE_FROM_CART: {
+            const item_index = action.index;
+             new_state = [...state];
+            new_state.splice(item_index, 1);
+            return new_state;
+        }
+
+        case CLEAR_CART: {
+             new_state = {...state};
+            new_state = [];
+            return new_state;
+        }
+
+        default: {
+            if(state === undefined)
+                return [];
+            
+            return state;
+        }
+    }
+}
